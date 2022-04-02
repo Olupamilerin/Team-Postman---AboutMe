6e74cb4f8415f447717e573e6d65f49a0236a52d
olupamilerin <fanawopop@gmail.com>
2022-04-01 22:29:28 -0700

    UPDATED LESSON 14 TASK

---
+++ TESTCOPECAMP/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World. I've been learning Software testing at TESTIFY Ltd and it's been awesome");
+    }
+}
